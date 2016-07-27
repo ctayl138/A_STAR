907c24421e98cd8d1abbb5b1b8d6bec9762780ea
Cliff <enkentaylor@yahoo.com>
2016-07-27 11:42:09 -0400

    final changes, bugs were due to manhattan method being in admissible

--- src/a_star/A_STAR.java
+++ src/a_star/A_STAR.java
@@ -9,34 +9,34 @@ import java.util.Scanner;
 
 public class A_STAR {
     
-    private Node [][] map = new Node[15][15];
-    private Random rand = new Random( );
-    private Node start;
-    private Node end;
-    private Node current;
-    private int blockCount = 0;
+    private Node [][] map = new Node[15][15]; // holds node objects
+    private Random rand = new Random( ); // rng
+    private Node start; //stores start node
+    private Node end; //stores end node
+    private Node current; //stores currently selected node
+    private int blockCount = 0; //stores number of blocked nodes
     private final PriorityQueue openList = new PriorityQueue<>(225, (Node n, Node m) -> {
-        return n.getF() < m.getF() ? - 1: n.getF() > m.getF() ? 1 :0;
+        return n.getF() < m.getF() ? - 1: n.getF() > m.getF() ? 1 :0; // determines how the priority queue (min heap) sorts the nodes
     });
-    private ArrayList closedList = new ArrayList(225);
+    private ArrayList closedList = new ArrayList(225); //stores the visited nodes
     
     public A_STAR( ){
         for(int i = 0; i < 15; i++){
             for(int j = 0; j < 15; j++){
-                map[i][j] = new Node(i, j, 0);
+                map[i][j] = new Node(i, j, 0); //creates blank nodes
             }
         }
-        while(blockCount < 90){
+        while(blockCount < 24){
             int m = rand.nextInt(15);
             int n = rand.nextInt(15);
 
             if(map[m][n].type != 1){
-                map[m][n].type = 1;
-                blockCount++;
+                map[m][n].type = 1; //stores the blocked nodes
+                blockCount++; //counts blocked nodes
             }
         }
         
-        System.out.println("Initialized Board:");
+        System.out.println("Initialized Board:"); //outputs board
         for(int i = 0; i < 15; i++){
             for(int j = 0; j < 15; j++){
                 System.out.print(map[i][j].type + " ");
@@ -44,40 +44,6 @@ public class A_STAR {
             System.out.print("\n");
         }
     }
-    class Node {
-        private int row, col, f, g, h, type; private Node parent;
-  
-        public Node(int r, int c, int t){
-            row = r; col = c; type = t; parent = null; //type 0 is traverseable, 1 is not
-        }
-        //mutator methods to set values 
-        public void setF(){
-            f = g + h;
-        } 
-        public void setG(int value){
-            g = value;
-        } 
-        public void setH(int value){
-            h = value;
-        } 
-        public void setParent(Node n){
-            parent = n;
-        }
-        public void setType(int n){
-            type = n;
-        }
-        //accessor methods to get values 
-        public int getF(){return f;} public int getType() {return type;} public int getG(){ return g; } public int getH(){ return h; } public Node getParent(){ return parent; } public int getRow(){ return row; } public int getCol(){ return col; }
-        @Override
-        public boolean equals(Object in){ //typecast to Node 
-            Node n = (Node) in;
-            return row == n.getRow() && col == n.getCol();
-        }   
-        @Override
-        public String toString(){
-            return "Node: " + row + "_" + col;
-        }
-}
     
     public static void main(String[] args) {
         A_STAR astar = new A_STAR( );
@@ -118,7 +84,7 @@ public class A_STAR {
                 System.out.print("\n");
             }
         
-            astar.move( );
+            astar.move( ); //initiates A* algorithm
             System.out.println("Do you want to choose different start and end points?");
             choice = scan.next().charAt(0);
             astar.mapReset();
@@ -140,56 +106,56 @@ public class A_STAR {
     }
     
     public void move( ){
-        openList.add(start);
+        openList.add(start); // adds start node
         
-        while(true){
+        while(true){ //iterating block
 
-            current = (Node) openList.poll();
-            closedList.add(current);
+            current = (Node) openList.poll(); //pops first node 
+            closedList.add(current); //adds current node to closed list
 
             Node n;  
             
-            if(current.getRow() - 1 >= 0){
+            if(current.getRow() - 1 >= 0){ // checks nodes above current node
                 n = map[current.getRow()-1][current.getCol()];
                 update(n, 10); 
 
-                if(current.getCol() - 1 >= 0){                      
+                if(current.getCol() - 1 >= 0){ //check diagonal node to upper left                     
                     n = map[current.getRow()-1][current.getCol()-1];
                     update(n, 14); 
                 }
 
-                if(current.getCol()+1 < 15){
+                if(current.getCol()+1 < 15){ //check diagonal node to the right
                     n = map[current.getRow()-1][current.getCol()+1];
                     update(n, 14); 
                 }
             } 
 
-            if(current.getCol() - 1 >= 0){
+            if(current.getCol() - 1 >= 0){ //checks node to the left
                 n = map[current.getRow()][current.getCol()-1];
                 update(n, 10); 
             }
 
-            if(current.getCol()+1< 15){
+            if(current.getCol()+1< 15){ // checks node to the right
                 n = map[current.getRow()][current.getCol()+1];
                 update(n, 10); 
             }
 
-            if(current.getRow()+1< 15){
+            if(current.getRow()+1< 15){ //checks node to the bottom
                 n = map[current.getRow()+1][current.getCol()];
                 update(n, 10); 
 
-                if(current.getCol()-1 >= 0){
+                if(current.getCol()-1 >= 0){ // get node to the bottom left
                     n = map[current.getRow()+1][current.getCol()-1];
                     update(n, 14); 
                 }
                 
-                if(current.getCol()+ 1 < 15){
+                if(current.getCol()+ 1 < 15){ //get node to the bottom right
                    n = map[current.getRow()+1][current.getCol()+1];
-                    update(n, 14); 
+                   update(n, 14); 
                 }
             
             }
-            if(openList.isEmpty()){
+            if(openList.isEmpty()){//terminating condition
                 trace(end);
                 System.out.println("\nFinal Path:");
                 System.out.println("0 => traverseable");
@@ -208,16 +174,16 @@ public class A_STAR {
         }
     }
     public void update(Node n, int cost){
-        if(n == null||closedList.contains(n)||n.getType() == 1){return;}
+        if(n == null||closedList.contains(n)||n.getType() == 1){return;} //if node is null, already visited, or blocked, exit
         n.setH(heuristic(n));
-        if(openList.contains(n)){
+        if(openList.contains(n)){ //if node is in open list
            if(n.getG() + cost < current.getG()){
                current.setParent(n);
                current.setG(n.getG()+cost);
                current.setF();
            }
         }
-        if(!openList.contains(n)){
+        if(!openList.contains(n)){ //if node is not in the open list
             n.setParent(current);
             n.setG(current.getG() + cost);
             n.setF();
@@ -245,11 +211,11 @@ public class A_STAR {
     
     public int heuristic(Node n){
         int h;
-        h = (abs((n.getCol()-end.getCol()))+ abs((n.getRow() - end.getRow())))*10;
+        h = (abs((n.getCol()-end.getCol()))+ abs((n.getRow() - end.getRow())))*10;//calculates manhattan score
         return h;
     }
     
-    public void trace(Node n){
+    public void trace(Node n){ //traces the path from the end node to the start node
         current = n.getParent();
         if(current == null){System.out.println("\nNo path could be found!");return;}
         while(current != start){
@@ -257,4 +223,39 @@ public class A_STAR {
             current = current.getParent();
         }
     }
+    
+    class Node {
+        private int row, col, f, g, h, type; private Node parent;
+  
+        public Node(int r, int c, int t){
+            row = r; col = c; type = t; parent = null; //type 0 is traverseable, 1 is not
+        }
+        //mutator methods to set values 
+        public void setF(){
+            f = g + h;
+        } 
+        public void setG(int value){
+            g = value;
+        } 
+        public void setH(int value){
+            h = value;
+        } 
+        public void setParent(Node n){
+            parent = n;
+        }
+        public void setType(int n){
+            type = n;
+        }
+        //accessor methods to get values 
+        public int getF(){return f;} public int getType() {return type;} public int getG(){ return g; } public int getH(){ return h; } public Node getParent(){ return parent; } public int getRow(){ return row; } public int getCol(){ return col; }
+        @Override
+        public boolean equals(Object in){ //typecast to Node 
+            Node n = (Node) in;
+            return row == n.getRow() && col == n.getCol();
+        }   
+        @Override
+        public String toString(){
+            return "Node: " + row + "_" + col;
+        }
+}
 }
